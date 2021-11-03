527e4128669f53668f7d25195cadf4639c82f1b7
Protoman <Protoman@PROTOMAN>
2021-11-02 23:38:28 -0300

    Add project files.

---
+++ Foemulario3/Shared/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foemulario3.Shared
+{
+    public class Usuario
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O nome é obrigatório!")]
+        public string Nome { get; set; }
+
+        [Range(18, 120, ErrorMessage = "A idade deve ser maior que 18")]
+        public int Idade { get; set; }
+
+    }
+}
